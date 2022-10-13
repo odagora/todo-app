7768e30978b7825cb1e3346735304e1e90c2ad3b
Daniel <oscar.d.gonzalez@gmail.com>
2022-10-13 12:03:52 -0500

    :sparkles: add input search state management

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TodoSearch.css";
 
 export const TodoSearch = () => {
-  return <input className="todo-search" placeholder="Escribe tu tarea aquí" />;
+  const [search, setSearch] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  return (
+    <>
+      <input
+        className="todo-search"
+        placeholder="Escribe tu tarea aquí"
+        value={search}
+        onChange={handleInputChange}
+      />
+      <p>{search}</p>
+    </>
+  );
 };
