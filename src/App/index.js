844e8e9f4c2b0fa6145e9e2625e456b30716157a
Daniel <oscar.d.gonzalez@gmail.com>
2022-10-13 16:37:32 -0500

    :sparkles: add data persistence with localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import { AppUI } from "./AppUI";
 
-const defaultTodos = [
-  { id: 1, text: "Estudiar Reactjs", completed: false },
-  { id: 2, text: "Planchar la ropa", completed: false },
-  { id: 3, text: "Enviar información del proyecto", completed: false },
-];
-
 export const App = () => {
+  const localStorageTodos = localStorage.getItem("TODO_V1");
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem("TODO_V1", JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = useState(parsedTodos);
   const [searchValue, setSearchValue] = useState("");
-  const [todos, setTodos] = useState(defaultTodos);
+
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+  const totalTodos = todos.length;
 
   const searchedTodos = !searchValue
     ? todos
@@ -20,19 +27,22 @@ export const App = () => {
         return todoText.includes(searchText);
       });
 
-  const completedTodos = todos.filter((todo) => todo.completed).length;
-  const totalTodos = todos.length;
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem("TODO_V1", stringifiedTodos);
+    setTodos(newTodos);
+  };
 
   const completeTodo = (text) => {
     const newTodos = todos.map((todo) =>
       todo.text === text ? { ...todo, completed: true } : todo
     );
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
     const newTodos = todos.filter((todo) => todo.text !== text);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
